1d91f3ea00b1ed44152148ebdf15f9e4273515fd
Dan Magee <dkmagee@gmail.com>
2017-01-30 13:00:31 -0800

    Added PyMC.

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name='hlfred',
-    version='0.1',
+    version='0.2',
     packages=['hlfred', 'hlfred.commands', 'hlfred.tasks', 'hlfred.utils'],
     include_package_data=True,
     install_requires=[
         'Click',
+        'PyMC',
     ],
     entry_points='''
         [console_scripts]
